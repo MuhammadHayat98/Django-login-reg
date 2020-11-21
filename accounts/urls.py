2d93156e2cd4e55b69ece590d3823c8edda85d08
herreraosmar83 <osmar.herreraperez.945@my.csun.edu>
2020-11-21 11:23:19 -0800

    implmented blog home page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.loginPage, name="login"),
     path('register/', views.registerPage, name="register"),
-    path('initdb/', views.userReturn, name="initdb")
+    path('initdb/', views.userReturn, name="initdb"),
+    path('blog/', views.blogPage, name="blog")
 ]
