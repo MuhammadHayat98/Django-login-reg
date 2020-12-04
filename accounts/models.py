7d670ec9a3178eb3a4684203c4a8ec7d46fe163b
MuhammadHayat98 <muhammad.hayat.698@my.csun.edu>
2020-12-04 03:22:43 -0800

    added follower functionality to models

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,22 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from taggit.managers import TaggableManager
 from django.urls import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    following = models.ManyToManyField(User, related_name='following', blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 class Blog(models.Model):
     subject = models.CharField(max_length=200)
@@ -22,7 +37,10 @@ class Blog(models.Model):
     
 
 class Comment(models.Model):
-    pos_neg = models.CharField(max_length=100)
+    POS = 'Positive'
+    NEG = 'Negative'
+    CHOICES = [(POS, '👍'), (NEG, '👎')]
+    pos_neg = models.CharField(max_length=100, choices=CHOICES, default=POS)
     desc = models.TextField(null=True)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
